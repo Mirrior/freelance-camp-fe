8866a9d58a2d1a492f55f369ffe868c42c079f3b
Rustin Player <rjplayer@outlook.com>
2018-03-16 11:06:32 -0600

    Added several ';' in places they were missing

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomepageComponent } from './homepage/homepage.component'
-import { DocumentsComponent } from './documents/documents.component'
+import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { DocumentsComponent } from './documents/documents.component'
+import { DocumentsComponent } from './documents/documents.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
