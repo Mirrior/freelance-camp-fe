7655bf295192afcc0fe9c64410905c7a900af3a6
Rustin Player <rjplayer@outlook.com>
2018-03-21 14:02:58 -0600

    Added proposal show page, routes, and links to show pages in proposal list

--- app/app.module.ts
+++ app/app.module.ts
@@ -22,7 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
     HomepageComponent,
     DocumentsComponent,
     ProposalListComponent,
-    ProposalNewComponent
+    ProposalNewComponent,
     ProposalShowComponent
   ],
   bootstrap: [
